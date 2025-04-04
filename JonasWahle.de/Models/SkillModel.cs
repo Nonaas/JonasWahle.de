c1d8aed57d75ae1e74d1dd6b6bf3f009ae0dd2c9
Jonas Wahle <wahle.jonas@web.de>
2025-04-04 08:03:23 +0200

    Add project files.

---
+++ JonasWahle.de/Models/SkillModel.cs
@@ -0,0 +1,8 @@
+﻿namespace JonasWahle.de.Models
+{
+    public class SkillModel
+    {
+        public string Icon { get; set; } = string.Empty;
+        public string Text { get; set; } = string.Empty;
+    }
+}
