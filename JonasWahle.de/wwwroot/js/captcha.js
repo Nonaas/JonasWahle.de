5afcc87ca8ce2ca4c07b5377f110b0929f6e2db8
Jonas Wahle <wahle.jonas@web.de>
2025-04-04 09:12:06 +0200

    Add dedicated javascript for captcha

---
+++ JonasWahle.de/wwwroot/js/captcha.js
@@ -0,0 +1,14 @@
+﻿function reloadHCaptcha() {
+    if (window.hcaptcha) {
+        hcaptcha.render(document.querySelector('.h-captcha'));
+    }
+}
+
+function validateCaptcha() {
+    var captchaResponse = document.querySelector('textarea[name="h-captcha-response"]');
+    if (!captchaResponse || captchaResponse.value.trim() === "") {
+        document.getElementById("captcha-error").style.display = "block";
+        return false;
+    }
+    return true;
+}
