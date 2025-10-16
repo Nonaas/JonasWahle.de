14c5368e7cdd4020170eb5fa3c7812404031c914
Jonas Wahle <wahle.jonas@web.de>
2025-10-16 13:39:40 +0200

    Create Constants.cs

---
+++ JonasWahle.de.Domain/Utilities/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace JonasWahle.de.Domain.Utilities
+{
+    public static class Constants
+    {
+        public static class CookieKeys
+        {
+            public const string CookieConsent = "CookieConsent";
+        }
+    }
+}
