02be5a5e4fbb54be69317439577971645565b91a
Jonas Wahle <wahle.jonas@web.de>
2025-10-16 15:14:19 +0200

    Add cookie service

---
+++ JonasWahle.de.Domain/Interfaces/ICookieService.cs
@@ -0,0 +1,10 @@
+﻿
+namespace JonasWahle.de.Domain.Interfaces
+{
+    public interface ICookieService
+    {
+        Task DeleteCookieAsync(string key);
+        Task<string?> GetCookieAsync(string key);
+        Task SetCookieAsync(string key, string value, int expireDays);
+    }
+}
---
+++ JonasWahle.de.Domain/Services/CookieService.cs
@@ -0,0 +1,49 @@
+﻿using JonasWahle.de.Domain.Interfaces;
+using Microsoft.JSInterop;
+
+namespace JonasWahle.de.Domain.Services
+{
+    public class CookieService(IJSRuntime JsRuntime) : ICookieService
+    {
+        public async Task<string?> GetCookieAsync(string key)
+        {
+            try
+            {
+                string? cookie = null;
+                cookie = await JsRuntime.InvokeAsync<string>("getCookie", key);
+                return cookie;
+            }
+            catch (Exception)
+            {
+                /* Throw up to caller */
+                throw;
+            }
+        }
+
+        public async Task SetCookieAsync(string key, string value, int expireDays)
+        {
+            try
+            {
+                await JsRuntime.InvokeVoidAsync("setCookie", key, value, expireDays);
+            }
+            catch (Exception)
+            {
+                /* Throw up to caller */
+                throw;
+            }
+        }
+
+        public async Task DeleteCookieAsync(string key)
+        {
+            try
+            {
+                await JsRuntime.InvokeVoidAsync("deleteCookie", key);
+            }
+            catch (Exception)
+            {
+                /* Throw up to caller */
+                throw;
+            }
+        }
+    }
+}
