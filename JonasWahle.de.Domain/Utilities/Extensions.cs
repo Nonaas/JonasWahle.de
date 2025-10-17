814c137640902027a615eae3b25a8e4884052bcd
Jonas Wahle <wahle.jonas@web.de>
2025-10-17 23:40:07 +0200

    Create Extensions.cs

---
+++ JonasWahle.de.Domain/Utilities/Extensions.cs
@@ -0,0 +1,15 @@
+﻿namespace JonasWahle.de.Domain.Utilities
+{
+    public class Extensions
+    {
+        public static string ToPascalCase(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return str;
+            }
+
+            return char.ToUpper(str[0]) + str.Substring(1);
+        }
+    }
+}
