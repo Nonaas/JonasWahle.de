6831da8bb222714c8a7b7597fcfd798150b0cc00
Jonas Wahle <wahle.jonas@web.de>
2025-10-18 18:09:00 +0200

    Create DownloadItem.cs

---
+++ JonasWahle.de.Domain/Models/DownloadItem.cs
@@ -0,0 +1,20 @@
+﻿using JonasWahle.de.Domain.Enums;
+
+namespace JonasWahle.de.Domain.Models
+{
+    public class DownloadItem
+    {
+        public required string ImagePath { get; set; }
+        public required string BackgroundColor { get; set; }
+        public required string FontColor { get; set; }
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+        public required string Category { get; set; }
+        public required List<PlatformEnum> Platforms { get; set; }
+        public required DateTime ReleaseDate { get; set; }
+        public required string DownloadUrl { get; set; }
+
+        public List<string>? Tags { get; set; }
+        public string? GitHubUrl { get; set; }
+    }
+}
