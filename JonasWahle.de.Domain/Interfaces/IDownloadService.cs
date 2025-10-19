5db62b47c523876ce8c6be457d92cf8fb4be3287
Jonas Wahle <wahle.jonas@web.de>
2025-10-19 18:29:05 +0200

    Add download service and interface for DI

---
+++ JonasWahle.de.Domain/Interfaces/IDownloadService.cs
@@ -0,0 +1,14 @@
+﻿using JonasWahle.de.Data.Models;
+
+namespace JonasWahle.de.Domain.Interfaces
+{
+    public interface IDownloadService
+    {
+        Task<DownloadItem> CreateDownloadItemAsync(DownloadItem downloadItem);
+        Task DeleteDownloadItemAsync(Guid id);
+        Task<List<DownloadItem>> GetAllDownloadItemsAsync();
+        Task<DownloadItem?> GetDownloadItemByIdAsync(Guid id);
+        Task<List<DownloadItem>> GetDownloadItemsByCategoryAsync(string category);
+        Task<DownloadItem> UpdateDownloadItemAsync(DownloadItem downloadItem);
+    }
+}
---
+++ JonasWahle.de.Domain/Services/DownloadService.cs
@@ -0,0 +1,62 @@
+﻿using JonasWahle.de.Data;
+using JonasWahle.de.Data.Models;
+using JonasWahle.de.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace JonasWahle.de.Domain.Services
+{
+    public class DownloadService(IDbContextFactory<ApplicationContext> DbFactory) : IDownloadService
+    {
+        public async Task<List<DownloadItem>> GetAllDownloadItemsAsync()
+        {
+            using ApplicationContext context = await DbFactory.CreateDbContextAsync();
+            return await context.DownloadItems
+                .Where(x => x.IsActive)
+                .OrderByDescending(x => x.ReleaseDate)
+                .ToListAsync();
+        }
+
+        public async Task<DownloadItem?> GetDownloadItemByIdAsync(Guid id)
+        {
+            using ApplicationContext context = await DbFactory.CreateDbContextAsync();
+            return await context.DownloadItems
+                .FirstOrDefaultAsync(x => x.Id == id && x.IsActive);
+        }
+
+        public async Task<List<DownloadItem>> GetDownloadItemsByCategoryAsync(string category)
+        {
+            using ApplicationContext context = await DbFactory.CreateDbContextAsync();
+            return await context.DownloadItems
+                .Where(x => x.Category == category && x.IsActive)
+                .OrderByDescending(x => x.ReleaseDate)
+                .ToListAsync();
+        }
+
+        public async Task<DownloadItem> CreateDownloadItemAsync(DownloadItem downloadItem)
+        {
+            using ApplicationContext context = await DbFactory.CreateDbContextAsync();
+            context.DownloadItems.Add(downloadItem);
+            await context.SaveChangesAsync();
+            return downloadItem;
+        }
+
+        public async Task<DownloadItem> UpdateDownloadItemAsync(DownloadItem downloadItem)
+        {
+            using ApplicationContext context = await DbFactory.CreateDbContextAsync();
+            context.DownloadItems.Update(downloadItem);
+            await context.SaveChangesAsync();
+            return downloadItem;
+        }
+
+        public async Task DeleteDownloadItemAsync(Guid id)
+        {
+            using ApplicationContext context = await DbFactory.CreateDbContextAsync();
+            DownloadItem? item = await context.DownloadItems.FindAsync(id);
+            if (item != null)
+            {
+                item.IsActive = false; // Soft delete
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
