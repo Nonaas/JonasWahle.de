bea2015d88fb6ffa3b5e60ee47ad9ac79910c33b
Jonas Wahle <wahle.jonas@web.de>
2025-10-20 16:12:03 +0200

    Add centralized AppTheme class

---
+++ JonasWahle.de.UI/Components/Layout/AppTheme.cs
@@ -0,0 +1,34 @@
+﻿using MudBlazor;
+
+namespace JonasWahle.de.UI.Components.Layout
+{
+    public class AppTheme : MudTheme
+    {
+        public static MudTheme Theme
+        {
+            get
+            {
+                return new() 
+                { 
+                    PaletteLight = _paletteLight,
+                    PaletteDark = _paletteDark
+                };
+            }
+        }
+
+        private static readonly PaletteLight _paletteLight = new()
+        {
+            Primary = new("#13263c"),
+            Secondary = new("#ee595b"),
+            Tertiary = new("#ffffff")
+        };
+
+        private static readonly PaletteDark _paletteDark = new()
+        {
+            Primary = new("#ee595b"),
+            Secondary = new("#13263c"),
+            Tertiary = new("#ffffff")
+        };
+
+    }
+}
